cb2cfba8c9ce32d0bf9a25eec64f204fda754a81
ARUZENCE <Andrejs.Ruzencevs@dbschenker.com>
2020-01-08 15:09:53 -0500

    BOOM

--- lib/widgets/TabFour.dart
+++ lib/widgets/TabFour.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+
+var card = Card(
+  elevation: 16,
+  child: Center(
+    child: Text('Sample'),
+  ),
+);
 var grid = GridView.count(
   primary: false,
   padding: const EdgeInsets.all(20),
@@ -7,116 +14,25 @@ var grid = GridView.count(
   mainAxisSpacing: 10,
   crossAxisCount: 2,
   children: <Widget>[
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('He\'d have yoasd sad sad sau all unravel at the'),
-      color: Colors.teal[100],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Heed not the rabble'),
-      color: Colors.teal[200],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Sound of screams but the'),
-      color: Colors.teal[300],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Who scream'),
-      color: Colors.teal[400],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution is coming...'),
-      color: Colors.teal[500],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Who scream'),
-      color: Colors.teal[400],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution is coming...'),
-      color: Colors.teal[500],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Who scream'),
-      color: Colors.teal[400],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution is coming...'),
-      color: Colors.teal[500],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Who scream'),
-      color: Colors.teal[400],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution is coming...'),
-      color: Colors.teal[500],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
-    Container(
-      padding: const EdgeInsets.all(8),
-      child: const Text('Revolution, they...'),
-      color: Colors.teal[600],
-    ),
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
+    card,
   ],
 );
 
